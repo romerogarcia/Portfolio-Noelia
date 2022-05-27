40e1da8a7e52a0e62b2823567368207600b2a883
Noelia R <noeliaromga@gmail.com>
2022-05-27 17:57:46 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import Portafolio from './components/Portafolio/Portafolio';
 import Contact from './components/Contact/Contact.js';
 
 import './index.css';
-
+const basename =
+  document.querySelector('base')?.getAttribute('href') ??
+  'https://romerogarcia.github.io/Portfolio-Noelia/';
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
