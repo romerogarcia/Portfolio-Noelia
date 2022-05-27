64175f264a0861bcd302fac95f633af5ca522a64
Noelia R <noeliaromga@gmail.com>
2022-05-27 16:57:01 +0200

    add docs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './App.js'
-import About from './components/About/About.js'
-import Portafolio from './components/Portafolio/Portafolio'
-import Contact from './components/Contact/Contact.js'
+import Home from './App.js';
+import About from './components/About/About.js';
+import Portafolio from './components/Portafolio/Portafolio';
+import Contact from './components/Contact/Contact.js';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='about' element={<About/>} />
-            <Route path='portafolio' element={<Portafolio />} />
-            <Route path='contact' element={<Contact/>} />
-        </Routes>
-    </BrowserRouter>
-    
-    
-    , 
-    document.getElementById('root')
-); 
+  <BrowserRouter>
+    <Routes basename={process.env.PUBLIC_URL}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="portafolio" element={<Portafolio />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>,
+
+  document.getElementById('root')
+);
